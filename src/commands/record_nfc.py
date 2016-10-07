f8c58d8febddb66551e30ac559dbbbe2e9d46891
Łukasz Lechowicz <llechowicz@gmail.com>
2016-10-07 22:27:00 +0100

    Add a custom command for the NFC bots.

---
+++ src/commands/record_nfc.py
@@ -0,0 +1,42 @@
+import random
+
+from .base import BaseCommand
+from .record import RecordCommand
+
+
+class RecordNFCCommand(BaseCommand):
+    """
+    This command can works only if the sender is one of the NFC bots
+    """
+    command_term = ':nfc:'
+
+    @property
+    def nfc_bots(self):
+        return [
+            self.poolbot.config['nfc_london'],  # Ted Lowe
+            self.poolbot.config['nfc_bristol'],  # Clive Everton
+        ]
+
+    def process_request(self, message):
+        """
+        The message author has to be one of the NFC bots (Bristol or London one).
+        The winner is mentioned first, then the loser.
+        """
+        msg_author = message['user']
+        if msg_author not in self.nfc_bots:
+            return
+
+        winner, loser = self._find_players(message['text'])
+
+        # Fake the record command
+        record = RecordCommand()
+        victory_noun = random.choice(record.victory_nouns)
+        message['user'] = winner
+        message['text'] = "{} <@{}>".format(victory_noun, loser)
+
+        # Pretend it's a record command!
+        return record.process_request(message)
+
+    def _find_players(self, text):
+        """Parses the message text and returns two user IDs: winner's and loser's."""
+        return self._find_players(text)[:2]
