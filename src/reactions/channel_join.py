e40bacaf49fcd374fe6f1221907a1eacad718fb2
Danny Milsom <dannysmilsom@gmail.com>
2017-01-02 20:54:08 +0000

    Fix the channel join reaction
    * Post a message when a user joins the channel.
    * If they have already played a game this season, post their leaderboard position.

--- src/reactions/channel_join.py
+++ src/reactions/channel_join.py
@@ -1,27 +1,58 @@
 from base import BaseReaction
 
+from utils import get_ordinal_extension
+
 
 class ChannelJoinReaction(BaseReaction):
+    """Welcome new users to the channel with poolbot monitoring messages."""
 
-    url_path = 'api/player'
+    url_path = 'api/player/'
 
     def match_request(self, message):
+        """Look for a message to represent a player joining the channel."""
         return message.get('subtype') == 'channel_join'
 
     def process_request(self, message):
-        user_id = self._find_subtype_mentions(message)[0]
-        user_details = self.poolbot.client.api_call('users.info', user=user_id)
+        """Find the message author ID, add them to the datastore if not already
+        registered and return a welcome message."""
+        user_id = message['user']
 
-        # add the user to the users list if not already
+        # add the user to the users list if not already saved
         if user_id not in self.poolbot.users:
             response = self.poolbot.session.post(
                 self._generate_url(),
                 data={
-                    'name': user_details['user']['name'],
-                    'slack_id': user_details['user']['id']
+                    'name': message['user_profile']['name'],
+                    'slack_id': user_id
                 }
             )
 
-        return 'Welcome to the baze {name}!'.format(
-            name=user_details['user']['name']
-        )
+            # create a local version of the user in the cache
+            player_profile = response.json()
+            
+            user_profile = flatten_nested_dict(message['user_profile'])
+            user_profile.update(player_profile)
+            self.poolbot.users[user_id] = User(**user_profile)
+
+            # generate our reply for the new user
+            return self.reply(
+                'Welcome to the baze {name}! :wave:'.format(
+                    name=self.poolbot.users[user_id].username
+                ),
+                callbacks=['help']
+            )
+
+        # if the user is already recognised get their leaderboard position
+        position = self.poolbot.get_leaderboard_position(message['user'])
+        username = self.poolbot.users[user_id].username
+
+        if position is None:
+            callbacks = []
+            msg = 'Welcome home {name}! :wave:'.format(name=username)
+        else:
+            callbacks = ['stats']
+            msg = 'Welcome back {name}! :wave: You currently hold {position} place in this seasons leaderboard!'.format(
+                name=username,
+                position=get_ordinal_extension(position)
+            )
+        return self.reply(msg, callbacks=callbacks)
