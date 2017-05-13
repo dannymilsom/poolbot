c6fac077df9a459ddc92b371e69c383b9ce03cf2
Danny Milsom <dannysmilsom@gmail.com>
2017-05-13 21:15:23 +0100

    Support passing a `minimum` argument to only include players with more than 10 games in the leaderboard

--- src/commands/leaderboard.py
+++ src/commands/leaderboard.py
@@ -2,22 +2,26 @@ from .base import BaseCommand
 
 
 class LeaderboardCommand(BaseCommand):
-    """Returns a leadboard of players, ordered by their wins."""
+    """Returns a leadboard of players, ordered by their elo points."""
 
     default_limit = 10
     default_elo_field = 'season'
     command_term = 'leaderboard'
     url_path = 'api/player/'
     help_message = (
-        'The leadboard command returns a table of users ranking by their raw '
-        'win count. You can pass `season` or `all` in the command to determine '
+        'The leadboard command returns a table of users ranking by their elo '
+        'points. You can pass `season` or `all` in the command to determine '
         'which elo points score should be used. If no additional argument is '
-        'passed, the season elo score is default.'
+        'passed, the season elo score is default. To reduce the length of the '
+        'leaderboard displayed, you can pass an additional integer argument. '
+        'To only include players who have played more than ten games, pass the'
+        '`minimum` argument.'
     )
 
     def process_request(self, message):
         """Get the elo scores via the API and format to form the leaderboard."""
-        self.elo_field = self._determine_elo_field(message)
+        command_args = self._command_args(message, include_user_mentions=False)
+        self.elo_field = self._determine_elo_field(command_args)
 
         leaderboard_url = self._generate_url()
         get_params = {
@@ -30,42 +34,48 @@ class LeaderboardCommand(BaseCommand):
         )
 
         if response.status_code == 200:
-            limit = self._calculate_limit(message)
-            return self.reply(self._generate_response(response.json(), limit))
+            limit = self._calculate_limit(command_args)
+            minimum = self._minimum_games_filter(command_args)
+            return self.reply(
+                self._generate_response(response.json(), limit, minimum)
+            )
         else:
             return self.reply('Unable to get leadboard data')
 
-    def _determine_elo_field(self, message):
+    def _determine_elo_field(self, command_args):
         """Determine which elo ranking field to use."""
-        args = self._command_args(message)
-        if args and args[0] == 'all':
-            elo_field = 'total'
-        else:
-            elo_field = self.default_elo_field
-        return elo_field
+        return 'total' if 'all' in command_args else self.default_elo_field
 
-    def _calculate_limit(self, message):
+    def _calculate_limit(self, command_args):
         """Parse the message to see if an additional parameter was passed
         to limit the number of players shown in the leaderboard. If no arg
         is passed, or the arg cannot be cast to an integer, default to 10.
         """
-        args = self._command_args(message)
-        for arg in args:
+        for arg in command_args:
             try:
                 return int(arg)
             except ValueError:
                 pass
         return self.default_limit
 
-    def _generate_response(self, data, limit):
+    def _minimum_games_filter(self, command_args):
+        """Determine if an argument was passed to filter the leaderboard to
+        only include players who have played a minimum number of games.
+        """
+        return 10 if 'minimum' in command_args else 1
+
+    def _generate_response(self, data, limit, minimum_games=1):
         """Parse the returned data and generate a string which takes the form
         of a leaderboard style table, with players ranked from 1 to X.
         """
         leaderboard_row_msg = '{ranking}. {name} [Elo Score: {elo}] ({wins} W / {losses} L)'
         leaderboard_table_rows = []
 
-        for player in data:  # Go through list
-            if player['{}_win_count'.format(self.elo_field)] or player['{}_loss_count'.format(self.elo_field)]:  # See if user has played any games
+        for player in data:
+            if (
+                player['{}_win_count'.format(self.elo_field)] >= minimum_games or
+                player['{}_loss_count'.format(self.elo_field)] >= minimum_games
+            ):
                 leaderboard_table_rows.append(leaderboard_row_msg.format(
                     ranking=len(leaderboard_table_rows) + 1,
                     name=player['name'],
